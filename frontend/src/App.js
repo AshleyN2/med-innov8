932eef4e43381eba225956b975cde8b002832b62
Ashley <ashleynjeri7@gmail.com>
2022-12-07 10:32:10 +0300

    Installed Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch("/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return <h1>Hello from Med-Innov8!</h1>;
 }
 
 export default App;
