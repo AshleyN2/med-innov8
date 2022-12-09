a26e00014bb48ed06b24e65f12673e918c199c9e
Ashley <ashleynjeri7@gmail.com>
2022-12-09 15:16:04 +0300

    Footer Component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,66 @@
+<>
+  {/*FOOTER*/}
+  <footer>
+    <div className="footer-top">
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-md-4">
+            <img className="logo" src="2logo.png" alt="" />
+          </div>
+          <div className="col-md-2">
+            <h5 className="text-black">Who We Are</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Board</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h5 className="text-black">More</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#">FAQ's</a>
+              </li>
+              <li>
+                <a href="#">Privacy &amp; Policy</a>
+              </li>
+              <li>
+                <a href="#">T's &amp; C's</a>
+              </li>
+              <li>
+                <a href="#">Career Portal</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <h5 className="text-black">Contact</h5>
+            <ul className="list-unstyled">
+              <li className="text-black">
+                Address: 5471 Daktari Street, Kenya
+              </li>
+              <li className="text-black">Email: support@medinnov8.ke</li>
+              <li className="text-black">Phone: 010-2839-1938</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="footer-bottom">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">ⓒ 2022 Created by Njeri .A. Njoroge</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  {/*FOOTER ENDS HERE*/}
+</>
